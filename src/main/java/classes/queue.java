7b80a90a1af84013f24e8eccfbe96aa713aad4e4
Louis <carreno.luisdavid@gmail.com>
2023-05-25 09:28:33 -0400

    queue implementation

---
+++ src/main/java/classes/queue.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author carre
+ */
+public class queue {
+    private Nodo first;
+    int size;
+    public queue(){
+        first = null;
+        size =0;
+    }
+}
